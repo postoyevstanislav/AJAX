1c0915060f0ec102dfb382647450ef76dc3ed6f2
staspostoyev <mr.postoyev@gmail.com>
2020-03-12 16:42:55 +0100

    no message

---
+++ xhr.js
@@ -0,0 +1,42 @@
+const user = {
+    user: {
+        name: 'Katya',
+        age: 23,
+        friends: [
+            "Soyka"
+        ]
+    }
+}
+
+let jsonStringify = JSON.stringify(user)
+console.log('jsonString: ', jsonStringify)
+
+let userParse = JSON.parse(jsonStringify)
+console.log('userCopy: ', userParse)
+
+//глибокe копiювання JSON.parse(JSON.stringify(user)) 
+
+function ajax(method, url, successCb, errorCb) {
+    
+    const xhr = new XMLHttpRequest()
+    
+    xhr.open(method, url)
+
+    xhr.send()
+    
+    
+    xhr.addEventListener('load',successCb)
+    xhr.addEventListener('error',errorCb)
+}
+
+ajax('GET', 'https://en5e7i2ee39ns.x.pipedream.net', function() {
+        console.log('success: ', this.response)
+    })
+    
+ajax('GET', 'https://www.google.com', function() {  
+        console.log('success: ', this.response)
+    })
+    
+
+
+
